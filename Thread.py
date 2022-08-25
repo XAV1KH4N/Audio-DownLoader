de71bcdcc324cffbccbf40474009a0c7d139b567
XAV1KH4N <xavierkhan31@gmail.com>
2022-08-25 15:01:01 +0100

    Run funciton that can be stoped

--- Thread.py
+++ Thread.py
@@ -6,4 +6,15 @@ class Thread(threading.Thread):
         threading.Thread.__init__(self)
         self.name = name
 
+        self.name = name
+
+
+    def run(self):
+        try:
+            pass
+            #func goes here
+        finally:
+            print('ended')
+
+
 
