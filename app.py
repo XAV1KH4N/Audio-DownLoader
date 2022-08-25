dc5aae19bb0bec13b888f0bedf2bd10bb4f99ca1
XAV1KH4N <xavierkhan31@gmail.com>
2022-08-25 20:31:09 +0100

    Able to download a text file from website to client side

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+import os
+
+from Interface import Interface
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/download', methods=['POST', 'GET'])
+def download():
+    if request.method == "POST":
+        song = request.form['song']
+        if len(song) != 0:
+            interface = Interface()
+            interface.download(song)
+            return render_template('index.html', download=song)
+
+    return render_template('index.html')
+
+@app.route('/send', methods=['POST','GET'])
+def download_file():
+    return send_file("static/file.txt", as_attachment=True)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
