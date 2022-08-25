0106307bdd9582eb437e1465f388914f39e9716f
XAV1KH4N <xavierkhan31@gmail.com>
2022-08-25 11:17:11 +0100

    Search Error exception created

--- Search.py
+++ Search.py
@@ -9,9 +9,13 @@ class Search:
         title = title.replace(" ","+")
         html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + title)
         video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+        if len(video_ids) == 0:
+            raise SearchError("Could not search find anything")
         return video_ids[0]
 
-
+class SearchError(Exception):
+    def __init__(self, message):
+        self.message = message
 
 if __name__ == '__main__':
     search = Search()
