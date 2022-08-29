478f3ffd1cce8106281b720d5fabe075b2277042
XAV1KH4N <xavierkhan31@gmail.com>
2022-08-29 12:19:19 +0100

    Download link available only when song has finished downloaded

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 import threading
-import time
 
 from Downloader import Downloader
 from Interface import Interface
 from flask import Flask, render_template, request, send_file
-from flask_socketio import SocketIO, emit
-from flask import Flask
-from flask import render_template, redirect, url_for
-from flask import request
-from flask import Response
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -23,25 +18,25 @@ def download():
     if request.method == "POST":
         song = request.form['song']
         if len(song) != 0:
-            #interface = Interface()
-            #interface.download(song)
 
-            d = threading.Thread(target=thread_function, args=(1,))
+            d = threading.Thread(target=thread_function, args=(1,song))
             d.start()
+
             return render_template('loading.html', download=song)
 
     return render_template('index.html')
 
 @app.route('/send', methods=['POST','GET'])
-def download_file():
+def download_file(song="/riptide.mp4"):
     d = Downloader()
-    path = d.downloadPath + "/riptide.mp4"
+    path = d.downloadPath + song
     return send_file(path, as_attachment=True)\
 
-def thread_function(name):
-    time.sleep(5)
-    print("Emitting")
+def thread_function(name, song):
+    interface = Interface()
+    interface.download(song)
     socketio.emit('ready', {'data': 'got it!'})
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
